ded9becd5239f810b09c9bd687374a009241b27e
umer2581 <mohmmedumer866@gmail.com>
2024-05-30 09:48:55 +0530

    some function and keyword added

--- save-links-extentions/js/index.js
+++ save-links-extentions/js/index.js
@@ -1,17 +1,39 @@
 let myLeads=[]
+let oldLeads=[]
 
 const inputEl=document.getElementById('input-el')
 
 const inputBtn=document.getElementById('input-btn')
 const ulEl=document.getElementById('ul-el')
+const leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"))
+const deletebtn=document.getElementById('delete-btn')
+deletebtn.addEventListener('dblclick',function()
+{
+  // console.log("Double Clicked")
+  localStorage.clear()
+  myLeads=[]
+  render()
+
+}
+)
+// console.log(leadsFromLocalStorage)
+// localStorage.setItem("naaz","www.example.com")
+// console.log(localStorage.getItem("naaz"))
+if(leadsFromLocalStorage){
+  myLeads=leadsFromLocalStorage
+  render()
+}
+
+
 
 
 inputBtn.addEventListener("click",function(){
     // console.log("hello")
     myLeads.push(inputEl.value)
     inputEl.value=""
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
     renderItems()
-    // console.log(myLeads)
+    // console.log(localStorage.getItem("myLeads"))
 })
 function renderItems(){
 let listItems=" "
